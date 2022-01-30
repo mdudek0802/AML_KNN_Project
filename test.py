b1c1d596e7cd2871542df7c162e36498c1a72511
mdudek0802 <mcd81@uakron.edu>
2022-01-30 15:45:24 -0500

    Add Confusion Matrix

--- test.py
+++ test.py
@@ -2,7 +2,9 @@ from mpl_toolkits import mplot3d
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import neighbors
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay
 
 DATASET = "2015.csv"
 K_NEIGHBORS = 7
@@ -46,15 +48,22 @@ if __name__ == "__main__":
     data = pd.DataFrame(data)
     regions = data["Region"]
     for region in regions:
-        if region not in unique_regions and (region != 'North America' or region != 'Australia and New Zealand'):
+        if region not in unique_regions and (region != 'North America' and region != 'Australia and New Zealand'):
             unique_regions.append(region)
     
     print(len(unique_regions))
     print(unique_regions)
 
-    sub_data = data[["Country", "Region", "Happiness Score", "Economy (GDP per Capita)",
+    # sub_data = data[["Country", "Region", "Happiness Score", "Economy (GDP per Capita)",
+    #                 "Family", "Health (Life Expectancy)", "Freedom",
+    #                 "Trust (Government Corruption)", "Generosity"]]
+
+    sub_data = data[["Region", "Happiness Score", "Economy (GDP per Capita)",
                     "Family", "Health (Life Expectancy)", "Freedom",
                     "Trust (Government Corruption)", "Generosity"]]
+
+    sub_data = sub_data[sub_data["Region"] != 'North America']
+    sub_data = sub_data[sub_data["Region"] != 'Australia and New Zealand']
 
     for region in unique_regions:
         temp_region_data = sub_data[sub_data["Region"] == region]
@@ -63,7 +72,41 @@ if __name__ == "__main__":
         region_density.append(len(temp_region_data))
         region_data.append(temp_region_data)
 
-    plot_by_three_attributes(region_data, "Happiness Score", "Economy (GDP per Capita)", "Health (Life Expectancy)")
+    # plot_by_three_attributes(region_data, "Happiness Score", "Economy (GDP per Capita)", "Health (Life Expectancy)")
     # plot_by_two_attributes(region_data, "Happiness Score", "Economy (GDP per Capita)")
 
+    regions_train, regions_test = train_test_split(sub_data, train_size=0.7, random_state=0)
+    # print(regions_test)
+    # print(regions_train)
+    # print(regions_test.iloc[:, 0])
+    # print(regions_test.iloc[:, 1:])
+
+    classifier = KNeighborsClassifier(K_NEIGHBORS, metric="euclidean")
+    classifier.fit(regions_train.iloc[:, 1:], regions_train.iloc[:, 0])
+
+    predicted = classifier.predict(regions_test.iloc[:, 1:])
+    # print(predicted[0])
+    # print(predicted.iloc[:, 1])
+
+    # disp = ConfusionMatrixDisplay.from_estimator(
+    #         classifier,
+    #         regions_test.iloc[:, 1:],
+    #         predicted,
+    #         # display_labels=unique_regions,
+    #         cmap=plt.cm.Blues,
+    #         normalize=None,
+    #     )
+    # disp.ax_.set_title("Confusion Matrix")
+
+    disp = ConfusionMatrixDisplay.from_estimator(
+            classifier,
+            regions_train.iloc[:, 1:],
+            regions_train.iloc[:, 0],
+            # display_labels=unique_regions,
+            cmap=plt.cm.Blues,
+            normalize=None,
+        )
+    disp.ax_.set_title("Confusion Matrix")
+    
+    plt.show()
 
