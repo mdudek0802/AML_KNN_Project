9d15ec931ea5b379fb4960e7761281efc747d97a
mdudek0802 <mcd81@uakron.edu>
2022-01-30 16:03:04 -0500

    Added printouts for various variables
    Re-added how many instances for each class and total, also added printouts for % of training, and distance metric

--- test.py
+++ test.py
@@ -51,16 +51,14 @@ if __name__ == "__main__":
         if region not in unique_regions and (region != 'North America' and region != 'Australia and New Zealand'):
             unique_regions.append(region)
     
-    print(len(unique_regions))
-    print(unique_regions)
-
-    # sub_data = data[["Country", "Region", "Happiness Score", "Economy (GDP per Capita)",
-    #                 "Family", "Health (Life Expectancy)", "Freedom",
-    #                 "Trust (Government Corruption)", "Generosity"]]
+    # print(len(unique_regions))
+    # print(unique_regions)
 
     sub_data = data[["Region", "Happiness Score", "Economy (GDP per Capita)",
                     "Family", "Health (Life Expectancy)", "Freedom",
                     "Trust (Government Corruption)", "Generosity"]]
+
+    print("# of attributes: " + str(len(sub_data.columns) - 1))
 
     sub_data = sub_data[sub_data["Region"] != 'North America']
     sub_data = sub_data[sub_data["Region"] != 'Australia and New Zealand']
@@ -72,31 +70,30 @@ if __name__ == "__main__":
         region_density.append(len(temp_region_data))
         region_data.append(temp_region_data)
 
+    # Uncomment to show a plot of different attributes
     # plot_by_three_attributes(region_data, "Happiness Score", "Economy (GDP per Capita)", "Health (Life Expectancy)")
     # plot_by_two_attributes(region_data, "Happiness Score", "Economy (GDP per Capita)")
 
-    regions_train, regions_test = train_test_split(sub_data, train_size=0.7, random_state=0)
-    # print(regions_test)
-    # print(regions_train)
-    # print(regions_test.iloc[:, 0])
-    # print(regions_test.iloc[:, 1:])
+    i = 0
+    for region in unique_regions:
+        print("Class: " + region + " has " + str(region_density[i]) + " instances")
+        i += 1
+    print("For a total of: " + str(sum(region_density)) + " instances")
 
-    classifier = KNeighborsClassifier(K_NEIGHBORS, metric="euclidean")
+    train_size=0.7
+    print("Using: " + str(train_size * 100) + "% for training.")
+
+    # Use commented out version for reproducable splitage
+    # regions_train, regions_test = train_test_split(sub_data, train_size=train_size, random_state=0)
+    regions_train, regions_test = train_test_split(sub_data, train_size=train_size)
+
+    metric = "euclidean"
+    print("Using: " + str(metric) + " for the distance metric")
+
+    classifier = KNeighborsClassifier(K_NEIGHBORS, metric=metric)
     classifier.fit(regions_train.iloc[:, 1:], regions_train.iloc[:, 0])
 
     predicted = classifier.predict(regions_test.iloc[:, 1:])
-    # print(predicted[0])
-    # print(predicted.iloc[:, 1])
-
-    # disp = ConfusionMatrixDisplay.from_estimator(
-    #         classifier,
-    #         regions_test.iloc[:, 1:],
-    #         predicted,
-    #         # display_labels=unique_regions,
-    #         cmap=plt.cm.Blues,
-    #         normalize=None,
-    #     )
-    # disp.ax_.set_title("Confusion Matrix")
 
     disp = ConfusionMatrixDisplay.from_estimator(
             classifier,
@@ -107,6 +104,6 @@ if __name__ == "__main__":
             normalize=None,
         )
     disp.ax_.set_title("Confusion Matrix")
-    
+
     plt.show()
 
