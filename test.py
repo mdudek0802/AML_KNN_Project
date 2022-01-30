fce17f90c25937b7798134eada073e1abf48bde2
mdudek0802 <mcd81@uakron.edu>
2022-01-30 13:05:06 -0500

    Add testing code

---
+++ test.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from sklearn import neighbors
+
+DATASET = "2015.csv"
+K_NEIGHBORS = 7
+
+if __name__ == "__main__":
+    unique_regions = []
+    region_density = []
+    data = pd.read_csv(DATASET)
+    data = pd.DataFrame(data)
+    regions = data["Region"]
+    for region in regions:
+        if region not in unique_regions:
+            unique_regions.append(region)
+    
+    print(len(unique_regions))
+    print(unique_regions)
+
+    sub_data = data[["Region", "Happiness Score", "Economy (GDP per Capita)",
+                    "Family", "Health (Life Expectancy)", "Freedom",
+                    "Trust (Government Corruption)", "Generosity"]]
+
+    for region in unique_regions:
+        region_density.append(len(sub_data[sub_data["Region"] == region]))
+
+    print(region_density)
+    print("Total Density: " + str(sum(region_density)))
+    # print(data)
+    # print()
+
+    
